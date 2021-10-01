facffee4e0c3f61b3d8ede296be605f60947d0d8
Sheep <rainbowshipz@gmail.com>
2021-10-01 23:37:05 +0300

    TLDWorkshop 7.1

---
+++ XP/Properties/Resources.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace XP.Properties
+{
+	// Token: 0x02000007 RID: 7
+	[GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+	[DebuggerNonUserCode]
+	[CompilerGenerated]
+	internal class Resources
+	{
+		// Token: 0x06000042 RID: 66 RVA: 0x00002050 File Offset: 0x00000250
+		internal Resources()
+		{
+		}
+
+		// Token: 0x17000005 RID: 5
+		// (get) Token: 0x06000043 RID: 67 RVA: 0x00008629 File Offset: 0x00006829
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static ResourceManager ResourceManager
+		{
+			get
+			{
+				if (Resources.resourceMan == null)
+				{
+					Resources.resourceMan = new ResourceManager("XP.Properties.Resources", typeof(Resources).Assembly);
+				}
+				return Resources.resourceMan;
+			}
+		}
+
+		// Token: 0x17000006 RID: 6
+		// (get) Token: 0x06000044 RID: 68 RVA: 0x00008655 File Offset: 0x00006855
+		// (set) Token: 0x06000045 RID: 69 RVA: 0x0000865C File Offset: 0x0000685C
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		internal static CultureInfo Culture
+		{
+			get
+			{
+				return Resources.resourceCulture;
+			}
+			set
+			{
+				Resources.resourceCulture = value;
+			}
+		}
+
+		// Token: 0x17000007 RID: 7
+		// (get) Token: 0x06000046 RID: 70 RVA: 0x00008664 File Offset: 0x00006864
+		internal static Bitmap toolbox
+		{
+			get
+			{
+				return (Bitmap)Resources.ResourceManager.GetObject("toolbox", Resources.resourceCulture);
+			}
+		}
+
+		// Token: 0x0400004E RID: 78
+		private static ResourceManager resourceMan;
+
+		// Token: 0x0400004F RID: 79
+		private static CultureInfo resourceCulture;
+	}
+}
